9973b3d785f2c6f41082bfc7ad87c0a7c23b58f6
sir-bravin111 <mrbravin69@gmail.com>
2025-12-16 13:40:01 +0300

    Update mesfonctions.js

--- Ibrahim/mesfonctions.js
+++ Ibrahim/mesfonctions.js
@@ -32,7 +32,7 @@ module.exports.genererNomFichier = async (extension) => {
 /** ************ */
 module.exports.stick = async (buffer, author) => {
     var sticker = new Sticker(buffer, {
-        pack: 'WHATSAPP BOT',
+        pack: 'jeepers creeper ke',
         author: author,
         type: StickerTypes.FULL,
         categories: ['🤩', '🎉'],
